85a0e2ccc41a052fccdd6944b9918edf7944081e
igolub <goolub.igor@gmail.com>
2013-11-19 20:03:31 +0100

    #13 Ocu i ja da se upoznam!

---
+++ cross-platform/protected/config/main.golub.php
@@ -0,0 +1,63 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+return array(
+
+    // application components
+    'components' =>
+    array(
+        
+        'user' =>
+        array(
+            // enable cookie-based authentication
+            'allowAutoLogin' =>true,
+        ),
+        
+        'urlManager' => 
+        array(
+           'urlFormat' => 'path',
+           'urlSuffix' => '.html',
+           'showScriptName' => false,
+           'caseSensitive' => false,
+           'rules' => 
+           array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ),
+
+        'db' =>
+        array(
+            'connectionString' => 'mysql:host=localhost;dbname=ermex',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
+
+        'errorHandler' =>
+        array(
+            'errorAction' => 'site/error',
+        ),
+
+        'log' =>
+        array(
+            'class' => 'CLogRouter',
+            'routes' =>
+        array(
+            array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                    ),
+                ),
+            ),
+        ),
+
+        // Yii::app()->params['paramName']
+        'params' =>
+        array(
+            'adminEmail' => 'softlabistocnosarajevo@gmail.com',
+        ),
+);
