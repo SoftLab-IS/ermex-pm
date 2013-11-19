8ea906f64bce08f1d546d1d3e8d3eef053203358
Ilja Tešić <ilija.tesic@gmail.com>
2013-11-19 18:04:37 +0100

    #13 Upoznavanje sa githubom
     Vracen obicni index.php

--- cross-platform/protected/config/main.itesic.php
+++ cross-platform/protected/config/main.itesic.php
@@ -3,26 +3,6 @@
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 return array(
-    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'Ermex PM',
-    'preload' => array('log'),
-
-    // autoloading model and component classes
-    'import' => 
-    array(
-        'application.models.*',
-        'application.components.*',
-    ),
-
-    'modules' => 
-    array(
-        'gii' =>
-        array(
-            'class' => 'system.gii.GiiModule',
-            'password' => 'admin',
-            'ipFilters' => array('127.0.0.1','::1'),
-        ),
-    ),
 
     // application components
     'components' =>
@@ -56,8 +36,8 @@ return array(
             'password' => 'icefire007',
             'charset' => 'utf8',
         ),
-        
-        'errorHandler' => 
+
+        'errorHandler' =>
         array(
             'errorAction' => 'site/error',
         ),
--- cross-platform/protected/config/main.milansamardzic.php
+++ cross-platform/protected/config/main.milansamardzic.php
@@ -3,27 +3,6 @@
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 return array(
-    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'Ermex PM',
-    'preload' => array('log'),
-
-    // autoloading model and component classes
-    'import' => 
-    array(
-        'application.models.*',
-        'application.components.*',
-    ),
-
-    'modules' => 
-    array(
-        'gii' =>
-        array(
-            'class' => 'system.gii.GiiModule',
-            'password' => 'admin',
-            'ipFilters' => array('127.0.0.1','::1'),
-        ),
-    ),
-
     // application components
     'components' =>
     array(
