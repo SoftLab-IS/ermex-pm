58741cbc1cd767e9c715ac928a2a210d11e91c12
Aleksandar Panic <arekusanda1@gmail.com>
2013-11-23 17:28:36 +0100

    Popravka UserIdentity

--- cross-platform/protected/components/UserIdentity.php
+++ cross-platform/protected/components/UserIdentity.php
@@ -24,7 +24,6 @@ class UserIdentity extends CUserIdentity
 			'admin'=>'admin',
 		);
 
-
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
 		elseif($users[$this->username]!==$this->password)
