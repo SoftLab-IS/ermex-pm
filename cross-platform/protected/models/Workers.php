8ac333a67062394979b9a9727466984b47a733b6
Aleksandar Panic <arekusanda1@gmail.com>
2013-11-25 18:28:42 +0100

    #33 U bazi nedostaje tabela za praćenje ko je šta radio

---
+++ cross-platform/protected/models/Workers.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * This is the model class for table "epm_workers".
+ *
+ * The followings are the available columns in table 'epm_workers':
+ * @property integer $woId
+ * @property integer $workAccountId
+ * @property integer $userId
+ * @property string $assignDate
+ * @property string $dueDate
+ * @property string $role
+ * @property integer $done
+ *
+ * The followings are the available model relations:
+ * @property WorkAccounts $workAccount
+ * @property Users $user
+ */
+class Workers extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'epm_workers';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('workAccountId, userId, assignDate, role', 'required'),
+			array('workAccountId, userId, done', 'numerical', 'integerOnly'=>true),
+			array('assignDate, dueDate', 'length', 'max'=>21),
+			array('role', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('woId, workAccountId, userId, assignDate, dueDate, role, done', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'workAccount' => array(self::BELONGS_TO, 'WorkAccounts', 'workAccountId'),
+			'user' => array(self::BELONGS_TO, 'Users', 'userId'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'woId' => 'Wo',
+			'workAccountId' => 'Work Account',
+			'userId' => 'User',
+			'assignDate' => 'Assign Date',
+			'dueDate' => 'Due Date',
+			'role' => 'Role',
+			'done' => 'Done',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('woId',$this->woId);
+		$criteria->compare('workAccountId',$this->workAccountId);
+		$criteria->compare('userId',$this->userId);
+		$criteria->compare('assignDate',$this->assignDate,true);
+		$criteria->compare('dueDate',$this->dueDate,true);
+		$criteria->compare('role',$this->role,true);
+		$criteria->compare('done',$this->done);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Workers the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
