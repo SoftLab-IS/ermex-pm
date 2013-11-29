f666cd4522d2339071df996e4524af72bb6cb33b
Milan <krunicmilan@yahoo.com>
2013-11-29 22:02:05 +0100

    #44 Tag naslova u pojedinim vjuovima iz H1 u H2 Tagovi promjenjeni

--- cross-platform/protected/views/radniNalozi/create.php
+++ cross-platform/protected/views/radniNalozi/create.php
@@ -13,6 +13,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Kreirenje novog radnog naloga</h1>
+<h2>Kreirenje novog radnog naloga</h2>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
--- cross-platform/protected/views/radniNalozi/view.php
+++ cross-platform/protected/views/radniNalozi/view.php
@@ -16,7 +16,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>Prikaz radnog naloga br. <?php echo $model->woId; ?></h1>
+<h2>Prikaz radnog naloga br. <?php echo $model->woId; ?></h2>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
