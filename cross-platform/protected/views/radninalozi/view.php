aeec24f5362099296024dae17c58ceb54230c421
Milan <krunicmilan@yahoo.com>
2013-12-08 15:52:00 +0100

    Radni nalozi view

--- cross-platform/protected/views/radninalozi/view.php
+++ cross-platform/protected/views/radninalozi/view.php
@@ -17,28 +17,59 @@ $this->menu=array(
 ?>
 <section>
 	<h2>Prikaz radnog naloga br. <?php echo $model->woId; ?></h2>
-	<div class="clearfix">
-		<p><?php $model->workAccountSerial ?></p>
-		<div style="float:right;">
-			<p><?php echo date('d.m.Y.',$model->creationDate) ?></p>
-			<p><?php echo $model->author->realName ?></p>
-		</div>
-	</div>
-	<div class="clearfix">
-		<div class="columns large-7">
-			<h3 class="columns large-12"><?php echo $model->payeeName ?></h3>
-			<h3 class="columns large-12"><?php echo $model->payeeContactInfo ?></h3>
-		</div>
-		<div class="columns large-5">
-			<p class="columns large-12"> 
-				<?php foreach ($usersList as $user) 
-				{
-					echo $user->realName . " " . $user->realSurname . "<br/>";
-				} ?> 
-			</p>
+	
+	<div class="clearfix panel">
+		<p class="columns large-4"><?php echo $model->workAccountSerial ?></p>
+		<div class="columns large-8" >
+			<p><?php echo date('d.m.Y.', $model->creationDate)?></p>
+			<p><?php echo  $model->author->realName . " " . $model->author->realSurname ?></p>
 		</div>
 	</div>
 	
+	<div class="clearfix panel">
+		<div class="columns large-5">
+			<h3 class="columns large-12"><?php echo $model->payeeName ?></h3>
+			<h3 class="columns large-12"><?php echo $model->payeeContactInfo ?></h3>
+		</div>
+		<div class="columns large-3">
+			<h3> <?php echo $model->additional ?> </h3>
+		</div>
+		<div class="columns large-4">
+			<ol>
+				<?php foreach ($usersList as $user) 
+				{
+					echo "<li>" . $user->realName . " " . $user->realSurname . "</li>";
+				} ?> 
+			</ol>
+		</div>
+	</div>	
+	
+	<div class="clearfix panel">
+		<h2>Narudžbe: </h2>
+		<div class="columns large-12">
+			<ol>
+				<?php foreach ($model->order as $order) 
+				{
+					echo "<li>" . $order->title . "</li>";	
+				} ?>
+			</ol>
+		</div>
+	</div>
+	
+	<div class="clearfix panel">
+		<h2>Materijali: </h2>
+		<div class="columns large-12">
+			<ul>
+				<?php var_dump($model); ?>
+				
+			</ul>
+		</div>
+	</div>
+	
+	<div class="clearfix panel">
+		<h2>Napomena:</h2>
+		<h2><?php echo $model->note ?> </h2>
+	</div>
 	
 </section>
 
